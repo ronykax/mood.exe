c9a610f52365fdde68687bf3057a5dd5bee0d730
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-05-02 03:12:18 +0530

    optional entry area

---
+++ src/components/Modal.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+
+export default function Modal({
+    onBackgroundClick,
+}: {
+    onBackgroundClick: () => void;
+}) {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.focus();
+        }
+    }, []);
+
+    return (
+        <div className="w-full h-full bg-black/75 fixed top-0 right-0 backdrop-blur-xl">
+            <div className="flex flex-col h-full p-4 gap-4 items-center">
+                <textarea
+                    ref={textareaRef}
+                    className="w-full h-full bg-white/10 rounded-md resize-none p-4 text-xl"
+                    placeholder="spill the tea if you must"
+                />
+
+                <div className="opacity-75 font-extralight text-xs flex justify-between w-full">
+                    <span>
+                        {new Date().toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                        })}
+                    </span>
+                    <span>
+                        press <span className="font-mono bg-white/20 rounded-xs px-0.5">enter</span> to skip
+                        or submit
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
