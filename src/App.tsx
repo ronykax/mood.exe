5bd71b1e23e1766fc7502d176efe999ca17ef327
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-05-02 03:12:51 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,85 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import {
+    isPermissionGranted,
+    requestPermission,
+} from "@tauri-apps/plugin-notification";
+
+import { TrayIcon } from "@tauri-apps/api/tray";
+import Modal from "./components/Modal";
+import { getMoodImages } from "./utils";
+
 export default function App() {
-    interface Mood {
-        emoji: string;
-        color: string;
+    const [pumpNotifications, shouldPumpNotifications] = useState(false);
+
+    useEffect(() => {
+        async function trayIcon() {
+            await TrayIcon.new({
+                tooltip: "hi world!",
+                title: "Hello, world!",
+            });
+        }
+
+        async function notifications() {
+            const granted = await isPermissionGranted();
+
+            if (granted) {
+                shouldPumpNotifications(true);
+            } else {
+                const req = await requestPermission();
+
+                if (req === "granted") {
+                    shouldPumpNotifications(true);
+                }
+            }
+        }
+
+        notifications();
+        trayIcon();
+    }, []);
+
+    useEffect(() => {
+        // setInterval(async () => {
+        // }, 5000);
+        // if (pumpNotifications) {
+        //     sendNotification({ title: "mood.exe", body: "hi world!" });
+        // }
+    }, [pumpNotifications]);
+
+    const [showModal, setShowModal] = useState(false);
+
+    function handleClick() {
+        setShowModal(true);
     }
 
-    type MoodGroup = Mood[][];
+    const moods = getMoodImages();
 
-    const moodGroup: MoodGroup = [
-        [
-            { emoji: "😞", color: "#fb2c36" },
-            { emoji: "😐", color: "#ffdf20" },
-            { emoji: "🙂", color: "#7bf1a8" },
-            { emoji: "😃", color: "#51a2ff" },
-        ],
-    ];
-
-    const moods = moodGroup[Math.floor(Math.random() * moodGroup.length)];
+    console.log(moods);
 
     return (
-        <div className="grid grid-cols-2 text-6xl w-full h-screen bg-black">
-            {moods.map(({ emoji, color }) => (
-                <button
-                    key={emoji}
-                    className={`cursor-pointer relative group`}
-                    style={{
-                        background: `radial-gradient(circle, ${color}ff 25%, #000 200%)`,
-                    }}
-                >
-                    <div className={`p-2 w-fit mx-auto ${color} rounded-full`}>
-                        {emoji}
-                    </div>
+        <div className="w-full h-screen relative">
+            <div className="grid grid-cols-2 w-full h-full bg-black">
+                {moods.map((src, i) => (
+                    <button
+                        key={i}
+                        onClick={handleClick}
+                        className={`cursor-pointer relative group`}
+                    >
+                        <img className="w-full h-full" src={src} alt="Mood" />
 
-                    <div className="animated-border absolute w-full h-full top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-150"></div>
-                </button>
-            ))}
+                        <div className="animated-border absolute w-full h-full top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 bg-black/0 group-hover:bg-black/50 flex items-center justify-center backdrop-blur-xs">
+                            <span className="text-2xl font-semibold text-white">
+                                {src.split("/").pop()?.slice(0, -7)}
+                            </span>
+                        </div>
+                    </button>
+                ))}
+            </div>
+
+            {showModal && (
+                <Modal onBackgroundClick={() => setShowModal(false)} />
+            )}
         </div>
     );
 }
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+export function getMoodImages(): string[] {
+    const moods = ["crying", "frown", "neutral", "happy"] as const;
+    const assets = import.meta.glob("/src/assets/moods/*.{png,gif}", {
+        eager: true,
+        import: "default",
+    });
+
+    const files = Object.entries(assets); // [path, imported image src]
+    const result: string[] = [];
+
+    for (const mood of moods) {
+        const matching = files.filter(([path]) =>
+            path.includes(`/moods/${mood}`)
+        );
+        if (matching.length === 0) continue;
+
+        const [_, imageSrc] =
+            matching[Math.floor(Math.random() * matching.length)];
+        result.push(imageSrc as string);
+    }
+
+    return result;
+}
