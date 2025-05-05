367f43408aea3267b9587f1b7ad74699c64e309b
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-05-06 00:58:20 +0530

    add plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,6 +19,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
