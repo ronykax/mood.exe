2f69cfaa8b214bbbd565cf19f008c5e522fb9fad
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-05-09 01:58:04 +0530

    fix ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { getMoodImages, getMoodName } from "$lib";
     import { submitEntry } from "$lib/submit-entry";
+    import { hide } from "@tauri-apps/api/app";
     import { onMount } from "svelte";
     import { fade } from "svelte/transition";
 
@@ -27,12 +28,20 @@
             mood: selectedMood,
         });
 
-        // alert that entry is submitted
+        // hide window
+        await hide();
+    }
+
+    let selectedIndex = $state<number | null>(null);
+
+    function handleClick(item: string, index: number) {
+        selectedMood = getMoodName(item) || "";
+        selectedIndex = index;
     }
 </script>
 
 <main
-    class="w-screen h-screen text-white p-1 bg-gradient-to-r from-stone-500 to-stone-700"
+    class="w-screen h-screen text-white p-2 bg-gradient-to-r from-stone-500 to-stone-700"
 >
     {#if showStartMsg}
         <div
@@ -49,16 +58,18 @@
         </div>
     {/if}
 
-    <div
-        class="flex flex-col w-full h-full rounded-[6px] overflow-hidden gap-1"
-    >
-        <div class="flex gap-1 w-full">
+    <div class="flex flex-col w-full h-full overflow-hidden gap-2">
+        <div class="flex gap-2 w-full">
             {#each moods as item, index}
                 {#if !showStartMsg && startAnimation}
                     <button
-                        class="w-full h-full aspect-square cursor-pointer relative overflow-hidden group"
+                        class="w-full h-full aspect-square rounded-xl cursor-pointer relative overflow-hidden group bg-black duration-200 {selectedIndex === index && 'border-[8px]'} {index === 2
+                            ? 'border-green-400/50'
+                            : index === 1
+                              ? 'border-yellow-300/50'
+                              : index === 0 && 'border-red-500/50'}"
                         in:fade={{ delay: (index + 1) * 200, duration: 600 }}
-                        onclick={() => (selectedMood = getMoodName(item) || "")}
+                        onclick={() => handleClick(item, index)}
                     >
                         <img
                             class="w-full h-full object-cover group-hover:scale-[112%] duration-150"
@@ -68,7 +79,7 @@
                         />
 
                         <div
-                            class="w-full h-full absolute top-0 right-0 flex justify-center items-center opacity-0 group-hover:opacity-100 group-hover:bg-black/50 group-hover:backdrop-blur-xs duration-150"
+                            class={`w-full h-full absolute top-0 right-0 flex justify-center items-center opacity-0 ${selectedIndex !== index && "group-hover:opacity-100 group-hover:bg-black/65 group-hover:backdrop-blur-xs"} duration-150 ${selectedIndex === null || selectedIndex === index ? "opacity-0" : "opacity-100 bg-black/65 backdrop-blur-xs"}`}
                         >
                             <span
                                 class="text-4xl font-semibold"
@@ -82,15 +93,21 @@
             {/each}
         </div>
 
-        <div class="flex w-full h-full bg-black/65">
-            <input
+        <div class="flex w-full h-full">
+            <!-- <input
                 type="text"
                 class="w-full h-full focus:outline-none px-6 placeholder:italic"
                 {placeholder}
                 bind:value={inputEntry}
-            />
+            /> -->
 
-            <button class="px-4 cursor-pointer" onclick={submit}>✅</button>
+            <textarea
+                class="resize-none w-full rounded-md h-full bg-black/50 focus:outline-none p-5 placeholder:italic text-xl"
+                {placeholder}
+                bind:value={inputEntry}
+            ></textarea>
+
+            <!-- <button class="px-4 cursor-pointer" onclick={submit}>✅</button> -->
         </div>
     </div>
 </main>
